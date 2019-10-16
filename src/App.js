276a2e0b2c9dc20e9070489b25241a5c467a30ab
Emiliano Cussino Caramello <1006633@ucc.edu.ar>
2019-10-16 17:53:24 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Album from "./components/Album";
+import SingIn from "./components/SingIn.js";
+function App() {
+  return (<Album/>);
+}
+function Iniciarses(){
+  return (<SingIn/>);
+}
+
+export default Album;
