85bef1edb26af12c8e36726d3e4f75aa1686ab32
Emiliano Cussino Caramello <1006633@ucc.edu.ar>
2019-11-12 19:20:07 -0300

    mejorando version estable

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Iniciarses from './App';
+import AppRouter from './routes'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Iniciarses/>, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
